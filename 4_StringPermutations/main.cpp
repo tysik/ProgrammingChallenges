8671e73413b03bb5e3931ebad96be48541593cf2
Mateusz Przybyla <inz.mateusz.przybyla@gmail.com>
2017-05-06 11:43:21 +0200

    Added permutations challenge.

---
+++ 4_StringPermutations/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int count = 0;
+
+void findPermutations(const string prefix, string str) {
+  if (str.length() == 0) {
+    count++;
+    cout << prefix << endl;
+    return;
+  }
+
+  for (int idx = 0; idx < str.length(); ++idx) {
+    string new_prefix(prefix);
+    string new_str(str);
+
+    new_prefix.push_back(str[idx]);
+    new_str.erase(idx, 1);
+
+    findPermutations(new_prefix, new_str);
+  }
+}
+
+void findPermutations(string str) {
+  findPermutations("", str);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cerr << "Incorrect number of arguments \n";
+    cerr << "Usage: \n";
+    cerr << "\t 4_StringPermutations string" << endl;
+    exit(1);
+  }
+
+  string base_string(argv[1]);
+
+  findPermutations(base_string);
+
+  cout << "Found " << count << " permutations!" << endl;
+
+  return 0;
+}
